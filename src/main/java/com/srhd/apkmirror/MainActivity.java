ff75065157f62892e5d930012ab4b033e716b62b
SkyrilHD <skyrilhd.info@gmail.com>
2020-03-14 21:39:58 +0100

    Support for older Androids
    After quite sometime, I finally got the project back and I am now maintaining this project again. :)
    
    Changelog:
    
    - my S4 (GT-I9506) 4.3 doesn't crash anymore -> permissions for older versions now supported
    - removed unused import statements
    
    Current bugs:
    
    - WebView is acting weird
    - it doesn't allow downloading files

--- src/main/java/com/srhd/apkmirror/MainActivity.java
+++ src/main/java/com/srhd/apkmirror/MainActivity.java
@@ -1,16 +1,12 @@
 package com.srhd.apkmirror;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.view.ActionMode;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
@@ -24,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Berechtigungen();
+        permissions();
 
         webView = (WebView) findViewById(R.id.APKMirror_Loader);
         webView.setWebViewClient(new WebViewClient());
@@ -41,23 +37,48 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void Berechtigungen(){
-        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+    public void permissions() {
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+
             if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-                Toast.makeText(this, "'Reading to External Storage'-Permission Denied", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "'Reading to External Storage'-Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "'Reading to External Storage'-Permission Granted", Toast.LENGTH_SHORT).show();
             }
-        } else {
-            Toast.makeText(this, "'Reading to External Storage'-Permission Granted", Toast.LENGTH_SHORT).show();
-        }
-        
-        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-                Toast.makeText(this, "'Writing to External Storage'-Permission Denied", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "'Writing to External Storage'-Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "'Writing to External Storage'-Permission Granted", Toast.LENGTH_SHORT).show();
             }
-        } else {
-            Toast.makeText(this, "'Writing to External Storage'-Permission Granted", Toast.LENGTH_SHORT).show();
+
+        }
+
+        if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "'Reading to External Storage'-Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "'Reading to External Storage'-Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "'Writing to External Storage'-Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "'Writing to External Storage'-Permission Granted", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
